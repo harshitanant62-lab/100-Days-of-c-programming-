87d76eaf5f62d21e72d6b9cd668d4ca37fe9bf46
harshitanant62-lab <harshitanant62@gmail.com>
2026-09-22 10:32:49 +0530

    Create day5.2.c

---
+++ day5.2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int totalSeconds, hours, minutes, seconds;
+
+    scanf("%d", &totalSeconds);
+
+    hours = totalSeconds / 3600;
+    totalSeconds = totalSeconds % 3600;
+
+    minutes = totalSeconds / 60;
+    seconds = totalSeconds % 60;
+
+    printf("%d:%d:%d", hours, minutes, seconds);
+
+    return 0;
+}
